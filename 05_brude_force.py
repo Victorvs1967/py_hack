f66c2f888d0986944d6d5a120e529262e9b039c1
Victorvs1967 <victorsmirnov@me.com>
2019-10-25 23:38:00 +0300

    add datafiles

--- 05_brude_force.py
+++ 05_brude_force.py
@@ -27,5 +27,5 @@ status_code = 0
 while status_code != 200:
     password = get_next_password()
     print(password)
-    r = requests.post('http://127.0.0.1:5000/auth', data={'login': 'admin', 'passwodd': password})
+    r = requests.post('http://127.0.0.1:5000/auth', data={'login': 'cat', 'passwodd': password})
     status_code = r.status_code
